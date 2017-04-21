0b4af907916f516d1cb540fddc27f85190cb887a
z.acharki <acharki.zakaria@gmail.com>
2017-04-21 16:24:24 +0100

    change tests mocha

--- test/config.js
+++ test/config.js
@@ -1,26 +1,64 @@
 'use strict';
 
-var path         = require('path');
-var fixtures     = require('haraka-test-fixtures');
+var assert = require('assert');
+var fixtures = require('haraka-test-fixtures');
+var Connection   = fixtures.connection;
 
-var _set_up = function (done) {
-    this.plugin = new fixtures.plugin('index');
-    this.plugin.config = this.plugin.config.module_config(path.resolve('test'));
-    done();
-};
+/*
+ var _set_up = function (done) {
+ this.plugin = new fixtures.plugin('index');
+ this.plugin.config = this.plugin.config.module_config(path.resolve('test'));
+ done();
+ };
+ */
 
-var default_config = {
-    main: {},
-    mta1: { ip: "1.1.1.1", host: "host1" },
-    mta2: { ip: "2.2.2.2", host: "hos" }
-};
+/*
+ exports.plugin_setup = {
+ setUp : _set_up,
+ 'registers': function (test) {
+ test.expect(1);
+ this.plugin.register();
+ test.deepEqual(this.plugin.cfg, default_config); // loaded config
+ test.done();
+ }
+ };*/
 
-exports.plugin_setup = {
-    setUp : _set_up,
-    'registers': function (test) {
-        test.expect(1);
-        this.plugin.register();
-        test.deepEqual(this.plugin.cfg, default_config); // loaded config
-        test.done();
-    }
-};
+describe('numbers check', function () {
+    it('register', function (done) {
+        var plugin = new fixtures.plugin('index');
+
+        var default_config = {
+            main: {},
+            mta1: { ip: "1.1.1.1", host: "host1" },
+            mta2: { ip: "2.2.2.2", host: "hos" }
+        };
+
+        plugin.register();
+        assert.deepEqual(this.plugin.cfg, default_config); // loaded config
+        done();
+    });
+
+    it('loads number.ini', function (done) {
+        done();
+    });
+
+    it.skip('mocks up a wss socket', function (done) {
+        done();
+    });
+
+    it.skip('inherits from haraka-plugin-redis', function (done) {
+        done();
+    });
+
+    it.skip('mocks up a connection', function (done) {
+        done();
+    });
+
+    it.skip('saves some results', function (done) {
+        done();
+    });
+
+    it.skip('sees those results via redis connection subscription', function (done) {
+        done();
+    });
+});
