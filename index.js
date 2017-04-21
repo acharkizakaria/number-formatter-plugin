5d8c97929e0314baa46445c5a08f38ec07951b63
z.acharki <acharki.zakaria@gmail.com>
2017-04-21 16:50:37 +0100

    tests fixes

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 //------------
 // documentation via: `haraka -h number-formatter-plugin`
 
-var outbound	= require('./outbound');
-var constants   = require('haraka-constants');
-var ip          = require('ip').address(); //Main ip of local server
-var host 	    = require('os').hostname().replace(/\\/, '\\057').replace(/:/, '\\072'); //Server hostname
+//var outbound	= require('./outbound');
+//var constants   = require('haraka-constants');
+//var ip          = require('ip').address(); //Main ip of local server
+//var host 	    = require('os').hostname().replace(/\\/, '\\057').replace(/:/, '\\072'); //Server hostname
 var cfg;
 
 exports.register = function () {
