749dfdf75204929ebe7c0da2b84357b0bccfa95c
z.acharki <acharki.zakaria@gmail.com>
2017-04-21 17:07:59 +0100

    tests fixes...

--- test/config.js
+++ test/config.js
@@ -3,34 +3,11 @@
 var assert      = require('assert');
 var fixtures    = require('haraka-test-fixtures');
 
-/*
- var _set_up = function (done) {
- this.plugin = new fixtures.plugin('index');
- this.plugin.config = this.plugin.config.module_config(path.resolve('test'));
- done();
- };
- */
-
-/*
- exports.plugin_setup = {
- setUp : _set_up,
- 'registers': function (test) {
- test.expect(1);
- this.plugin.register();
- test.deepEqual(this.plugin.cfg, default_config); // loaded config
- test.done();
- }
- };*/
-
 describe('numbers check', function () {
     it('register', function (done) {
         var plugin = new fixtures.plugin('index');
 
-        var default_config = {
-            main: {},
-            mta1: { ip: "1.1.1.1", host: "host1" },
-            mta2: { ip: "2.2.2.2", host: "hos" }
-        };
+        var default_config = {main: {},mta1: { ip: "1.1.1.1", host: "host1" },mta2: { ip: "2.2.2.2", host: "hos" }};
 
         plugin.register();
         assert.deepEqual(plugin.cfg, default_config); // loaded config
